4ba36632a2b1cf661682d07c309519085eb96cb7
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-04-25 18:54:16 -0300

    method receivers

--- main/menu/menu.go
+++ main/menu/menu.go
@@ -12,22 +12,32 @@ type menuItem struct {
 	prices map[string](float64)
 }
 
-var in = bufio.NewReader(os.Stdin)
+type menu []menuItem
 
-// Adds an item to the menu
-func AddItem() {
-	fmt.Println("Please enter the name of the new item")
-	name, _ := in.ReadString('\n')
-	menu = append(menu, menuItem{name: name, prices: make(map[string]float64)})
-}
-
-// Print the menu for user interaction
-func Print() {
-	for _, item := range menu {
+func (m menu) print() {
+	for _, item := range m {
 		fmt.Println(item.name)
 		fmt.Println(strings.Repeat("-", 10))
 		for size, price := range item.prices {
 			fmt.Printf("\t%10s%10.2f\n", size, price)
 		}
 	}
+}
+
+func (m *menu) addItem() {
+	fmt.Println("Please enter the name of the new item")
+	name, _ := in.ReadString('\n')
+	*m = append(*m, menuItem{name: name, prices: make(map[string]float64)})
+}
+
+var in = bufio.NewReader(os.Stdin)
+
+// Adds an item to the menu
+func AddItem() {
+	data.addItem()
+}
+
+// Print the menu for user interaction
+func Print() {
+	data.print()
 }
